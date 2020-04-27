a2ee96060c71b899e3c6611a8589f57d29ed4070
javitocor <javitocor@gmail.com>
2020-04-27 06:59:00 +0200

    Created users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
